d8a859bd284cad689bfca0d7f761abb563f3a0ce
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-06-17 10:22:18 -0700

    adds copy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+
   return (
     <main>
       <h2>James 1st Deployed Web Page </h2>
