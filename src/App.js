eb76f9f01d8a36be8d7667c8ed6c66e24d769f17
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-06-17 21:41:49 -0700

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <h2>James 1st Deployed Web Page </h2>
-      <button>Give me the money!</button>
+      <button>Give me the money honey !</button>
       
         
     </main>
